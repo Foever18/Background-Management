31d47f77cc64c379406f887e35a4ed2daa64e8b0
Foever18 <1012163589@qq.com>
2022-08-24 18:09:41 +0800

    99

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -16,7 +16,7 @@
         </el-form-item>
         <el-form-item class="btn-item">
           <el-button type="primary" @click="login">登录888</el-button>
-          <el-button type="info" @click="reset">重置1</el-button>
+          <el-button type="info" @click="reset">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
