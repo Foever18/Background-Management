050c85bb4107d95486a43c8b0c46ad45c83e9919
Foever18 <105473892+Foever18@users.noreply.github.com>
2022-08-24 17:30:45 +0800

    Update index.vue

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -15,7 +15,7 @@
           ></el-input>
         </el-form-item>
         <el-form-item class="btn-item">
-          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="primary" @click="login">登录99</el-button>
           <el-button type="info" @click="reset">重置</el-button>
         </el-form-item>
       </el-form>
